002b1d8ecd77b06a3a352b02cc100fb8c9db8640
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-22 19:18:37 -0600

    Add playRound() to compare hands and determine winner

--- script.js
+++ script.js
@@ -23,9 +23,28 @@ function sanitizeChoice(choice) {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let winMessage = "YOU WIN! " + playerSelection + " beats " + computerSelection;
+    let loseMessage = "YOU LOSE, LOSER!! " + computerSelection + " beats " + playerSelection;
 
+    if (playerSelection == computerSelection) {
+        return "TIE! You both chose " + playerSelection;
+    }
+
+    // switch has ruled out ties, just need to check if you won, if not you lost
+    switch(playerSelection) {
+        case "rock":
+            return (computerSelection == "scissors") ? winMessage : loseMessage;
+        case "paper":
+            return (computerSelection == "rock") ? winMessage : loseMessage;
+        case "scissors":
+            return (computerSelection == "paper") ? winMessage : loseMessage;
+    }
+
+    console.log("How did you get here?");
 }
 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log("Computer randomly chose: " + computerSelection);
 
-
-console.log("Computer randomly chose: " + getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
