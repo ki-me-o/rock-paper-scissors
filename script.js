849d912f59894b62a95108bc8fa79f8612e797e0
Sebastian Subola <sebastian.subola@gmail.com>
2022-09-25 11:52:33 -0600

    Separate round logic and game state keeping

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const choices = document.querySelectorAll('.selections button');
 choices.forEach(button => button.addEventListener('click', playRound));
 
 /* Game Logic */
+const WIN = 1;
+const TIE = 0;
+const LOSE = -1;
+
+let playerSelection, computerSelection;
+let playerScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3)) + 1; // Random # between 1 and 3
     switch(choice){
@@ -27,29 +34,48 @@ function sanitizeChoice(choice) {
     
 }
 
-function playRound()  {
-    const computerSelection = getComputerChoice();
-    const playerSelection = this.classList.value;
-    const winMessage = "YOU WIN! " + playerSelection + " beats " + computerSelection;
-    const loseMessage = "YOU LOSE, LOSER!! " + computerSelection + " beats " + playerSelection;
+function updateMessage(message) {
+    messageText = document.querySelector('.message');
+    messageText.innerText = message;
+}
 
-    console.log('entering round, playerSelection: ' + playerSelection);
-    console.log('computerSelection: ' + computerSelection);
+function updateGameState(result) {
+    let message = "TIE";
+    winMessage = "YOU WIN! " + playerSelection + " beats " + computerSelection;
+    loseMessage = "YOU LOSE, LOSER!! " + computerSelection + " beats " + playerSelection;
+
+    if(result === WIN) {
+        message = winMessage;
+    } else if(result === LOSE) {
+        message = loseMessage;
+    }
+    console.log(message);
+    
+}
+
+// Return values: +1 player wins, 0 tie, -1 computer wins
+function playRound()  {
+    computerSelection = getComputerChoice();
+    playerSelection = this.classList.value;
+
+    console.log("Player choice: " + playerSelection + " and Computer choice: " + computerSelection);
 
     if (playerSelection == computerSelection) {
-        return "TIE! You both chose " + playerSelection;
+        updateGameState(TIE);
+        return;
     }
 
     // switch has ruled out ties, just need to check if you won, if not you lost
     switch(playerSelection) {
         case "rock":
-            return (computerSelection == "scissors") ? winMessage : loseMessage;
+            updateGameState ((computerSelection == "scissors") ? WIN : LOSE);
+            break;
         case "paper":
-            return (computerSelection == "rock") ? winMessage : loseMessage;
+            updateGameState ((computerSelection == "rock") ? WIN : LOSE);
+            break;
         case "scissors":
-            return (computerSelection == "paper") ? winMessage : loseMessage;
+            updateGameState ((computerSelection == "paper") ? WIN : LOSE);
+            break;
     }
-
-    console.log("How did you get here?");
 }
 
